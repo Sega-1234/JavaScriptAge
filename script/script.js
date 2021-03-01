da2d9a43240c016f34f08c9f94cf8933178bc9e7
sega-1234 <sedjkogay.2004@gmail.com>
2021-03-01 15:28:36 +0500

    four

--- script/script.js
+++ script/script.js
@@ -1,36 +1,12 @@
 
-// var name = prompt('Введите свое имя')
+var name = prompt('Введите свое имя')
 
-// var birt = +prompt('Введите свой год рождения')
+var birt = +prompt('Введите свой год рождения')
 
-// var year = +prompt('Введите нынешний год')
+var year = +prompt('Введите нынешний год')
 
-// if(birt < year) {
-//     alert(name + ', ' + 'Ваш возраст ' + (year - birt));
-// }else{
-//     alert('Ошибка');
-// }
-
-
-
-let sum = +prompt('Введите количество примеров')
-
-function random(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+if(birt < year) {
+    alert(name + ', ' + 'Ваш возраст ' + (year - birt));
+}else{
+    alert('Ошибка');
 }
-
-console.log(random(1,20));
-
-
-
-for(let i = 0; i = sum; i++){
-    let a = random(min, max)
-    let b = random(min, max)
-    let primer =+prompt(exx)
-}
-
-function exx(add, sub, mul, div ) {
-    return Math.random() * Math.floor(exx)
-}
-
-console.log(exx(a + ' + ' + b + ' =' == a + b , a + ' - ' + b + ' =' == a - b , a + ' * ' + b + ' =' == a * b , a + ' / ' + b + ' =' == a / b ));
